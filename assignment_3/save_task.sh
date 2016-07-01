c6414fa1f95dd83d9e5370583be5e6883c8a1bb7
DPF0190 <domenick.fuma[Dx[D[D[D[D[D[D[D@gmail.com>
2016-07-01 07:25:51 +0000

    save_task.sh

---
+++ assignment_3/save_task.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+
+newTask="when you see this the task text has been expanded"
+
+# double quotes are expanded, single are not
+echo $newTask
+echo "$newTask"
+echo '$newTask'
+
+echo "------------"
+echo "first arg: $1"
+echo "============"
+echo "first arg: $2"
+echo "============"
+echo "first arg: $3"
+
+newTask=$1
+echo "$newTask"
+
+

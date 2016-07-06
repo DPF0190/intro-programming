3613e5f36709fe8deb7fd0b5978a01b8b02ec896
DPF0190 <domenick.fuma[Dx[D[D[D[D[D[D[D@gmail.com>
2016-07-06 16:18:47 +0000

    save.py

---
+++ assignment_4/save.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+def parseArguments(argvArray):
+    """ Since sys.argv includes the name of the script itself, we omit
+    the first element and combine the rest into a single string. Then we
+    add a newline so appended tasks won't be bunched up."""
+    all_but_command_arg = argvArray[1:]
+    argsAsWord = " ".join(all_but_command_arg)
+    return argsAsWord + "\n"
+
+
+def saveTask(taskString):
+    """ writes passed taskString to the file database"""
+
+    dbFilename = "task_database_python.txt"
+    dbPath = os.path.join( os.getenv("HOME"), dbFilename )
+    with open(dbPath, "a") as myfile:
+        myfile.write(taskString)
+
+
+new_task= parseArguments(sys.argv)
+
+print(sys.argv[1: ])
+print(" ".join(sys.argv[1: ]))
+

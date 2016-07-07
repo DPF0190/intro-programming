fd8f478f05e740a1a8bc15844618563146fdf4d4
DPF0190 <domenick.fuma[Dx[D[D[D[D[D[D[D@gmail.com>
2016-07-07 11:14:59 +0000

    exercises.py

--- assignment_5/exercises.py
+++ assignment_5/exercises.py
@@ -1,30 +1,54 @@
 import math
 
-def gravity(gravity):
+
+
+def get_fall_time(vertical_height):
     # gravity isn't going to change, units in m/(s^2)
-    gravity = 9.8
+    acceleration_by_gravity = 9.8
 
-def height(height):
-    height = (1/2) * gravity * time * time
+    time_elapsed = math.sqrt((2 * vertical_height) / acceleration_by_gravity)
+    return time_elapsed
 
-def time(time):
-    time = math.sqrt((2 * height) / gravity)
-    return time    
-
-print(time(10))
-
-
-
-
-
-
-
-
-
+print(get_fall_time(10))
  
+  
+
+
+def isVulnerable(tower_height, tower_x , tower_y, target_x, target_y):
+    muzzle_velocity = 300
+
+    # update this line to calculate time_in_air using get_fall_time() function
+    time_in_air = get_fall_time(tower_height)
+
+    tower_range = time_in_air * muzzle_velocity
+
+    delta_x = tower_x - target_x
+    delta_y = tower_y - target_y
+
+    separation = math.sqrt((delta_x ** 2 ) + (delta_y ** 2)) 
+
+    if separation < tower_range: 
+        print("The target is closer than the tower range, what should we return?")
+        return True
+    else:         
+        print("The target is further than the tower range, what should we return?")
+        return False
+isVulnerable(10,9,8,7,6)
 
 
 
 
+def to_fahrenheit(degrees_celsius):
+    output = degrees_celsius * (9/5) + 32
+    return output
+          
+def to_celsius(degrees_fahrenheit):
+    output = (degrees_fahrenheit - 32) * 5/9
+    return output
+            
 
-
+print(to_fahrenheit(50))
+print(to_fahrenheit(150))
+print(to_celsius(0))
+print(to_celsius(50))
+print(to_celsius(150))
